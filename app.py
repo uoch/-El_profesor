f274761825014dddd2d28034402ab7db01d1efe5
chaouch <chaouch.thameurtc61@gmail.com>
2024-05-08 16:58:51 +0100

    done

--- app.py
+++ app.py
@@ -143,14 +143,12 @@ demo1 = gr.Interface(
     [gr.Audio(sources=["microphone"] , type="numpy"), gr.Image(
         type="filepath", label="Image")],
     gr.Image(type="pil", label="output Image"),
-    examples=[["the-king-and-three-sisters-around-the-world-stories-for-children.png"]]
 )
 demo2 = gr.Interface(
     run,
     [gr.Audio(sources=["upload"]), gr.Image(
         type="filepath", label="Image")],
-    gr.Image(type="pil", label="output Image"),
-    examples=[["the-king-and-three-sisters-around-the-world-stories-for-children.png"]]
+    gr.Image(type="pil", label="output Image")
 )
 with demo:
     gr.TabbedInterface([demo1, demo2],
