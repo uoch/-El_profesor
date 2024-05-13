8025b16336de4318a16daddbad913587a778fdd1
Thameur <chaouch@users.noreply.huggingface.co>
2024-05-13 16:23:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ description = """
 
 **How to Use:**
 
-1. **Record Yourself**: Begin by recording yourself speaking the content that corresponds to the text in the image. Ensure your pronunciation is clear and matches the text.
+1. **Record Yourself**: Begin by recording yourself speaking the content that corresponds to the text in the image. .
 
 2. **Upload Recorded Audio**: After recording, upload the audio file containing your speech. This audio will be used to enhance text extraction from the image.
 
@@ -21,7 +21,7 @@ description = """
 
 **Note:** This application aims to assist you in improving your ability to accurately transcribe spoken words from images. It may not provide perfect results in all cases, but it can help you track your progress and refine your transcription skills over time.
 """
-
+im = "the-king-and-three-sisters-around-the-world-stories-for-children.png"
 
 demo1 = gr.Interface(
     run,
@@ -37,7 +37,8 @@ demo2 = gr.Interface(
         type="filepath", label="Image")],
     [gr.Image(type="pil", label="output Image")],
     title=title,
-    description=description
+    description=description,
+    examples=[["Beginning.wav", im], ["Middels.wav", im]]
 )
 demo3 = gr.ChatInterface(
     respond,
@@ -70,10 +71,11 @@ demo4 = gr.Interface(
         gr.Textbox(label="Conversations", type="text")
     ],
     title="Document-based Chatbot",
+    examples=[[im, "how many sisters in the story", False]],
     description="This chatbot allows you to upload a document and ask questions. It can provide answers based on the content of the document as well as access information from the internet if enabled."
 )
 with demo:
-    gr.TabbedInterface([demo1, demo2, demo3, demo4], [
-                       "Microphone", "Audio File", "general_Chatbot", "Document_Chatbot"])
+    gr.TabbedInterface([demo2, demo4, demo1, demo3], [
+                       "Audio File", " Document_Chatbot", " Microphone", "general_Chatbot"])
 if __name__ == "__main__":
     demo.launch()
